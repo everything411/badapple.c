7a7c44625c0be1d4f4d2138f58cc867807f1b883
everything411 <everything411@163.com>
2018-10-19 12:34:04 +0800

    variable name change

--- player.c
+++ player.c
@@ -1,32 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#define DATAFILE "bad_apple.txt"
-#define MUSICFILE "bad_apple.mp3"
-#define USTODELAY 30000
-#define BUFLEN 10000
-#define CHARPERLINE 161
-#define LINEPERFRAME 61
-int main(void) {
+#include <fcntl.h>
+#define DATA_FILE "bad_apple.txt"
+#define MUSIC_FILE "bad_apple.mp3"
+#define US_TO_DELAY 30000
+#define BUF_LEN 10000
+#define CHAR_PER_LINE 161
+#define LINE_PER_FRAME 61
+int main(void)
+{
 	FILE *fp;
-	char buffer[BUFLEN] = { 0 };
-	if ((fp = fopen(DATAFILE, "r")) == 0) {
+	char buffer[BUF_LEN] = {0};
+	if ((fp = fopen(DATA_FILE, "r")) == 0)
+	{
 		return 1;
 	}
-	pid_t pid=fork();
-	if(pid==0)
+	pid_t pid = fork();
+	if (pid == 0)
 	{
-		execlp("mpg123", "mpg123", "-q", MUSICFILE, (char *)0);
+		execlp("mpg123", "mpg123", "-q", MUSIC_FILE, (char *)0);
 	}
-	while (1) {
-		for (int i = 0; i < LINEPERFRAME; i++) {
-			if (fgets(buffer + i * CHARPERLINE, CHARPERLINE + 1, fp) == NULL) {
+	while (1)
+	{
+		for (int i = 0; i < LINE_PER_FRAME; i++)
+		{
+			if (fgets(buffer + i * CHAR_PER_LINE, CHAR_PER_LINE + 1, fp) == NULL)
+			{
 				fclose(fp);
 				exit(0);
 			}
 		}
-      printf("[H%s", buffer);
-      buffer[0] = 0;
-      usleep(USTODELAY);
+		printf("[H%s", buffer);
+		buffer[0] = 0;
+		usleep(US_TO_DELAY);
 	}
 }
--- player2.c
+++ player2.c
@@ -2,31 +2,32 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#define DATAFILE "bad_apple.txt"
-#define MUSICFILE "bad_apple.mp3"
-#define USTODELAY 30000
-#define BUFLEN 10000
-#define CHARPERFRAME 9662
-int main(void) {
+#define DATA_FILE "bad_apple.txt"
+#define MUSIC_FILE "bad_apple.mp3"
+#define US_TO_DELAY 25500
+#define BUF_LEN 10000
+#define CHAR_PER_FRAME 9662
+int main(void)
+{
 	int fd;
-	char buffer[BUFLEN]={0};
-	if ((fd = open(DATAFILE,O_RDONLY))==-1)
+	char buffer[BUF_LEN] = {0};
+	if ((fd = open(DATA_FILE, O_RDONLY)) == -1)
 	{
 		return 1;
 	}
-	pid_t pid=fork();
-	if(pid==0)
+	pid_t pid = fork();
+	if (pid == 0)
 	{
-		execlp("mpg123", "mpg123", "-q", MUSICFILE, (char *)0);
+		execlp("mpg123", "mpg123", "-q", MUSIC_FILE, (char *)0);
 	}
 	while (1)
 	{
-		if(read(fd,buffer,CHARPERFRAME)!=CHARPERFRAME)
+		if (read(fd, buffer, CHAR_PER_FRAME) != CHAR_PER_FRAME)
 		{
 			close(fd);
 			return 0;
 		}
-		printf("[H%s",buffer);
-		usleep(USTODELAY);
+		printf("[H%s", buffer);
+		usleep(US_TO_DELAY);
 	}
 }
