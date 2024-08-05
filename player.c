3955c188048e29a569d3e6192edb9030aefe7f30
everything411 <everything411@163.com>
2024-08-05 22:37:15 +0800

    improve frame time

--- player.c
+++ player.c
@@ -2,12 +2,26 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+
 #define DATA_FILE "bad_apple.txt"
 #define MUSIC_FILE "bad_apple.mp3"
-#define US_TO_DELAY 30000
+#define US_TO_DELAY 300
+#define US_PRE_FRAME 33333
 #define BUF_LEN 10000
 #define CHAR_PER_LINE 161
-#define LINE_PER_FRAME 61
+#define LINE_PER_FRAME 60
+#define CLEAR_SCR "\x1b[H"
+
+// return a microsecond timestamp
+long long timestamp(void)
+{
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return tv.tv_sec * 1000000LL + tv.tv_usec;
+}
+
 int main(void)
 {
 	FILE *fp;
@@ -20,19 +34,40 @@ int main(void)
 	if (pid == 0)
 	{
 		execlp("mpg123", "mpg123", "-q", MUSIC_FILE, (char *)0);
+		exit(0); // if fail, exit
 	}
+	int size_read = 0;
+	long long start_time = timestamp();
+	long long frame = 1;
+	long long now_time = 0;
 	while (1)
 	{
-		for (int i = 0; i < LINE_PER_FRAME; i++)
+		size_read = fread(buffer, 1, CHAR_PER_LINE * LINE_PER_FRAME + 2, fp);
+		if(size_read == 0)
 		{
-			if (fgets(buffer + i * CHAR_PER_LINE, CHAR_PER_LINE + 1, fp) == NULL)
-			{
-				fclose(fp);
-				exit(0);
-			}
+			// EOF, stop
+			break;
 		}
-		printf("[H%s", buffer);
-		buffer[0] = 0;
-		usleep(US_TO_DELAY);
+		// CLEAR_SCR is a magic ASCII escape sequence \x1b[H
+		// when pressing HOME on keyboard it produces \x1b[H in tty and return the cursor to line 1 col 1
+		// we print the sequence to return to line 1 col 1
+		printf(CLEAR_SCR "%s\n", buffer);
+		while(1)
+		{
+			// if now - start > framecount * frametime
+			// then we reach the next frame
+			now_time = timestamp();
+			if(now_time - start_time > frame * US_PRE_FRAME)
+			{
+				printf("frame %lld\n", frame);
+				frame++;
+				break;
+			}
+			// sleep for a while instead of busy waiting to reduce cpu usage
+			usleep(US_TO_DELAY);
+		}
 	}
+	wait(NULL); // wait for mpg123
+	return 0;
 }
+
